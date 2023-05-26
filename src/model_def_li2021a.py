0a8632e1f897011543c2cc67a475b11e03963af9
ekepes112 <61498073+ekepes112@users.noreply.github.com>
2023-05-26 16:01:12 +0200

    functionality(modul_def_li2021a.py) added randomized input masking layer

--- src/model_def_li2021a.py
+++ src/model_def_li2021a.py
@@ -1,10 +1,48 @@
 from tensorflow import optimizers as opt
+from tensorflow.random import uniform
+from tensorflow import dtypes, concat, zeros, ones
 from keras.models import Model
-from keras.layers import Input, Dense, Conv1D, Flatten, BatchNormalization, Activation, GlobalAveragePooling1D, MaxPool1D
+from keras.layers import Input, Dense, Conv1D, Flatten, BatchNormalization, Activation, GlobalAveragePooling1D, MaxPool1D, Layer
 from keras.regularizers import l1_l2
 from warnings import warn
 from keras.initializers import HeNormal
 from modul_def_residual import residual_block
+
+
+class RandomMask(Layer):
+
+    def __init__(self, size, **kwargs):
+        super(RandomMask, self).__init__(**kwargs)
+        self.size = size
+
+    def call(self, inputs, training):
+        if not training:
+            return inputs
+        index = uniform(
+            shape=[],
+            minval=0,
+            maxval=inputs.shape[1]-self.size,
+            dtype=dtypes.int32,
+            seed=None,
+            name=None
+        )
+        mask = concat(
+            [ones(
+                [inputs.shape[0], index],
+                dtype=dtypes.float32
+            ),
+                zeros(
+                [inputs.shape[0], self.size],
+                dtype=dtypes.float32
+            ),
+                ones(
+                [inputs.shape[0], inputs.shape[1] - index - self.size],
+                dtype=dtypes.float32
+            )],
+            axis=1
+        )
+
+        return inputs * mask
 
 
 def compile_model(
@@ -16,13 +54,15 @@ def compile_model(
     loss_func: str = None,
     eval_metrics: list = [],
     model_id: str = 'prototype',
-    add_batch_norm: bool = True
+    add_batch_norm: bool = True,
+    mask_size: int = 1000
 ):
     model_id = f'li2021a_{model_id}'
     if optimizer is None:
         optimizer = opt.Adam(learning_rate=lr)
 
     model_input = Input(shape=input_shape)
+    model_input = RandomMask(size=mask_size)(model_input)
 
     x = Conv1D(
         filters=64,
