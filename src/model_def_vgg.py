3358eb8e77b6fb98daa150ed9300569baf0892ad
ekepes112 <61498073+ekepes112@users.noreply.github.com>
2023-06-21 10:42:40 +0200

    chore(residual_modules.py) refactored the model definition

--- src/model_def_vgg.py
+++ src/model_def_vgg.py
@@ -21,6 +21,9 @@ def compile_model(
     if loss_func is None:
         raise ValueError('No loss function specified')
 
+    conv_layer_counts = [2, 4, 4, 4]
+    conv_layer_sizes = [64, 128, 256, 256]
+
     model_input = Input(shape=input_shape)
     x = Conv1D(
         filters=32,  # lowered by a factor of 3 compared to the original work since we have 3 times fewer channels
@@ -39,116 +42,18 @@ def compile_model(
     x = MaxPool1D(
         pool_size=2,
     )(x)
-    x = Conv1D(
-        filters=64,  # see motivation above, the scaling is not exact
-        kernel_size=3,
-        strides=1,
-        padding='same',
-        activation='relu',
-    )(x)
-    x = Conv1D(
-        filters=64,  # see motivation above, the scaling is not exact
-        kernel_size=3,
-        strides=1,
-        padding='same',
-        activation='relu',
-    )(x)
-    x = MaxPool1D(
-        pool_size=2,
-    )(x)
-    x = Conv1D(
-        filters=128,  # see motivation above, the scaling is not exact
-        kernel_size=3,
-        strides=1,
-        padding='same',
-        activation='relu',
-    )(x)
-    x = Conv1D(
-        filters=128,  # see motivation above, the scaling is not exact
-        kernel_size=3,
-        strides=1,
-        padding='same',
-        activation='relu',
-    )(x)
-    x = Conv1D(
-        filters=128,  # see motivation above, the scaling is not exact
-        kernel_size=3,
-        strides=1,
-        padding='same',
-        activation='relu',
-    )(x)
-    x = Conv1D(
-        filters=128,  # see motivation above, the scaling is not exact
-        kernel_size=3,
-        strides=1,
-        padding='same',
-        activation='relu',
-    )(x)
-    x = MaxPool1D(
-        pool_size=2
-    )(x)
-    x = Conv1D(
-        filters=256,  # see motivation above, the scaling is not exact
-        kernel_size=3,
-        strides=1,
-        padding='same',
-        activation='relu',
-    )(x)
-    x = Conv1D(
-        filters=256,  # see motivation above, the scaling is not exact
-        kernel_size=3,
-        strides=1,
-        padding='same',
-        activation='relu',
-    )(x)
-    x = Conv1D(
-        filters=256,  # see motivation above, the scaling is not exact
-        kernel_size=3,
-        strides=1,
-        padding='same',
-        activation='relu',
-    )(x)
-    x = Conv1D(
-        filters=256,  # see motivation above, the scaling is not exact
-        kernel_size=3,
-        strides=1,
-        padding='same',
-        activation='relu',
-    )(x)
-    x = MaxPool1D(
-        pool_size=2
-    )(x)
-    x = Conv1D(
-        filters=256,  # see motivation above, the scaling is not exact
-        kernel_size=3,
-        strides=1,
-        padding='same',
-        activation='relu',
-    )(x)
-    x = Conv1D(
-        filters=256,  # see motivation above, the scaling is not exact
-        kernel_size=3,
-        strides=1,
-        padding='same',
-        activation='relu',
-    )(x)
-    x = Conv1D(
-        filters=256,  # see motivation above, the scaling is not exact
-        kernel_size=3,
-        strides=1,
-        padding='same',
-        activation='relu',
-    )(x)
-    x = Conv1D(
-        filters=256,  # see motivation above, the scaling is not exact
-        kernel_size=3,
-        strides=1,
-        padding='same',
-        activation='relu',
-    )(x)
-    x = MaxPool1D(
-        pool_size=2
-    )(x)
+    for conv_layer_count, conv_layer_size in zip(conv_layer_counts, conv_layer_sizes):
+        for _ in range(conv_layer_count):
+            x = Conv1D(
+                filters=conv_layer_size,  # see motivation above, the scaling is not exact
+                kernel_size=3,
+                strides=1,
+                padding='same',
+                activation='relu',
+            )(x)
+        x = MaxPool1D(
+            pool_size=2,
+        )(x)
 
     output = prediction_head(
         x,
@@ -163,8 +68,6 @@ def compile_model(
         name=model_id
     )
 
-    if not loss_func:
-        warn('Model compiled without loss function')
     model.compile(
         optimizer=optimizer,
         loss=loss_func,
