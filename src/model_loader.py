01de7e1eda5128960dd4405c08c9a4810ca0d079
ekepes112 <kepes.erik@gmail.com>
2023-07-10 16:14:51 +0200

    fix: return Callables rather than objects

--- src/model_loader.py
+++ src/model_loader.py
@@ -6,10 +6,10 @@ from model_def_resnet_bottleneck import ResNetBottleneck1D
 from model_def_resnet_simple import ResNetSimple1D
 
 models = {
-    'alexnet': AlexNet1D(),
-    'vgg': VGG1D(),
-    'resnet_bottleneck': ResNetBottleneck1D(),
-    'resnet_bn': ResNetBatchNorm1D(),
-    'resnet_simple': ResNetSimple1D(),
-    'densenet': DenseNet1D(),
+    'alexnet': AlexNet1D,
+    'vgg': VGG1D,
+    'resnet_bottleneck': ResNetBottleneck1D,
+    'resnet_bn': ResNetBatchNorm1D,
+    'resnet_simple': ResNetSimple1D,
+    'densenet': DenseNet1D,
 }
