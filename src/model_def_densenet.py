5ca35268b636b44ac084c1d60b67dc31fb4180e9
ekepes112 <kepes.erik@gmail.com>
2023-07-10 16:00:33 +0200

    chore: refactored model definition as class

--- src/model_def_densenet.py
+++ src/model_def_densenet.py
@@ -7,68 +7,83 @@ from residual_modules import residual_block_concat
 from ann_modules import prediction_head
 
 
-def compile_model(
-    l1_dense: float = 0.,
-    l2_dense: float = 0.,
-    input_shape: tuple = (None,),
-    optimizer: opt.Optimizer = None,
-    loss_func: str = None,
-    eval_metrics: list = [],
-    model_id: str = 'prototype',
-    prediction_sizes: list = [2048, 1024],
-):
+class DenseNet1D():
     # https://arxiv.org/pdf/1608.06993v5.pdf
-    model_id = f'densenet_{model_id}'
-    if optimizer is None:
-        optimizer = opt.Adam(learning_rate=3e-4)
-    if loss_func is None:
-        raise ValueError('No loss function specified')
+    def __init__(
+        self,
+        l1_dense: float = 0.,
+        l2_dense: float = 0.,
+        input_shape: tuple = (None,),
+        optimizer: opt.Optimizer = None,
+        loss_func: str = None,
+        eval_metrics: list = [],
+        model_id: str = 'prototype',
+        prediction_sizes: list = [2048, 1024],
+    ):
+        if loss_func is None:
+            raise ValueError('No loss function specified')
+        if optimizer is None:
+            print('No optimizer provided, adding default Adam with 3e-4 initial lr')
+            self.optimizer = opt.Adam(learning_rate=3e-4)
+        else:
+            self.optimizer = optimizer
+        self.l1_dense = l1_dense
+        self.l2_dense = l2_dense
+        self.input_shape = input_shape
+        self.loss_func = loss_func
+        self.eval_metrics = eval_metrics
+        self.model_id = model_id
+        self.prediction_sizes = prediction_sizes
+        self.model_id = f'densenet_{model_id}'
 
-    model_input = Input(shape=input_shape)
-    x = Conv1D(
-        kernel_size=7,
-        strides=2,
-        filters=32,
-    )(model_input)
-    x = MaxPool1D(
-        pool_size=3,
-        strides=2,
-    )(x)
-    for _ in range(4):
-        for _ in range(3):
-            x = residual_block_concat(x)
-        x = BatchNormalization()(x)
-        x = Activation('relu')(x)
+    def build(self):
+        model_input = Input(shape=self.input_shape)
         x = Conv1D(
-            kernel_size=1,
-            strides=1,
+            kernel_size=7,
+            strides=2,
             filters=32,
-        )(x)
-        x = AveragePooling1D(
-            pool_size=2,
+        )(model_input)
+        x = MaxPool1D(
+            pool_size=3,
             strides=2,
         )(x)
-    for _ in range(3):
-        x = residual_block_concat(x)
-    # x = GlobalAveragePooling1D()(x)
+        for _ in range(4):
+            for _ in range(3):
+                x = residual_block_concat(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+            x = Conv1D(
+                kernel_size=1,
+                strides=1,
+                filters=32,
+            )(x)
+            x = AveragePooling1D(
+                pool_size=2,
+                strides=2,
+            )(x)
+        for _ in range(3):
+            x = residual_block_concat(x)
+        # x = GlobalAveragePooling1D()(x)
 
-    output = prediction_head(
-        x,
-        layer_sizes=prediction_sizes,
-        lambda_l1=l1_dense,
-        lambda_l2=l2_dense,
-    )
+        output = prediction_head(
+            x,
+            layer_sizes=self.prediction_sizes,
+            lambda_l1=self.l1_dense,
+            lambda_l2=self.l2_dense,
+        )
 
-    model = Model(
-        model_input,
-        output,
-        name=model_id
-    )
+        model = Model(
+            model_input,
+            output,
+            name=self.model_id
+        )
 
-    model.compile(
-        optimizer=optimizer,
-        loss=loss_func,
-        metrics=eval_metrics
-    )
+        model.compile(
+            optimizer=self.optimizer,
+            loss=self.loss_func,
+            metrics=self.eval_metrics
+        )
 
-    return model
+        return model
+
+
