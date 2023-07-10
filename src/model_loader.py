fe39ddcb98335d2cf4e1dbc6530a9c454c0c8e40
ekepes112 <kepes.erik@gmail.com>
2023-07-10 16:13:18 +0200

    functionality: allows loading the models more concisely

--- src/model_loader.py
+++ src/model_loader.py
@@ -1,4 +1,9 @@
 from model_def_alexnet import AlexNet1D
+from model_def_densenet import DenseNet1D
+from model_def_vgg import VGG1D
+from model_def_resnet_bn import ResNetBatchNorm1D
+from model_def_resnet_bottleneck import ResNetBottleneck1D
+from model_def_resnet_simple import ResNetSimple1D
 
 models = {
     'alexnet': AlexNet1D(),
