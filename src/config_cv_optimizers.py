6c47c837b532a0e13cdbba595ee80df6b1793c8e
ekepes112 <kepes.erik@gmail.com>
2023-07-17 14:19:13 +0200

    functionality: constants specific for the optimizer cross validation

---
+++ src/config_cv_optimizers.py
@@ -0,0 +1,7 @@
+RESULTS_PATH = '/content/gdrive/My Drive/projects/convVsMlp/temp'
+LR_SCAN_END = 1e-1
+LR_SCAN_START = 1e-8
+LR_SCAN_STEP_SIZE = .2
+LR_SCAN_WARMUP = 10
+BATCH_SIZE = 64
+CV_EPOCHS = 250
