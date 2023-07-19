2d7874540d7c0abcbbf5385aafbc8787d8ecdb81
ekepes112 <61498073+ekepes112@users.noreply.github.com>
2023-07-19 16:18:53 +0200

    functionality: more flexible cv function calls

--- src/cv_optimizer.py
+++ src/cv_optimizer.py
@@ -1,10 +1,13 @@
 import argparse
+import ast
 import numpy as np
 import pandas as pd
+
 from pathlib import Path
 from tensorflow import Variable
 from tensorflow.keras.models import clone_model
 from tensorflow.keras.metrics import RootMeanSquaredError, MeanAbsoluteError
+from tensorflow.keras.callbacks import ModelCheckpoint
 import wandb
 from wandb.keras import WandbCallback
 
@@ -14,44 +17,58 @@ import cv_utils
 import config_cv_optimizers
 import learning_rate_estimator
 
+
 def cv_run(
     model_name: str,
     fold: int,
     compound: str,
     predictors_path: str,
     targets_path: str,
-    lr_scan_params: dict
+    lr_scan_params: dict,
+    callbacks: list,
+    checkpoint_dir: str,
+    start_checkpoint_at: float,
+    **kwargs
 ):
+    # prepare callbacks
+    selected_callbacks = [x.strip() for x in callbacks.split(',')]
+    callbacks = [cv_utils.ReinitializeWeights()]
     # load the data
     targets = pd.read_pickle(targets_path)
     predictors = pd.read_pickle(predictors_path)
+    # prepare model parameters
+    model_params = config.MODEL_PARAMS.get(model_name).copy()
+    model_params.update(
+        config.SHARED_MODEL_PARAMS
+    )
+    model_params.update({
+        'input_shape': (predictors.shape[1], 1)
+    })
+    model_params.update(kwargs)
     # define model architecture
     base_model = model_loader.models.get(
         model_name,
         'Invalid model name'
     )(
         model_id='optimizer_cv_',
-        **config.MODEL_PARAMS.get(model_name),
-        **config.SHARED_MODEL_PARAMS,
-        input_shape=(predictors.shape[1],1),
+        **model_params,
     ).build()
     # take the core model name
     base_model_name = base_model.name.split('_')[0]
-
     print(f'processing fold {fold}')
     # regenerate optimizers to reset their states
     explored_optimizers = cv_utils.generate_optimizers()
     # split the data names
     train_names = targets.loc[
-        targets.loc[:,f'{compound}_Folds'] != fold,:
+        targets.loc[:, f'{compound}_Folds'] != fold, :
     ].index
     val_names = targets.loc[
-        targets.loc[:,f'{compound}_Folds'] == fold,:
+        targets.loc[:, f'{compound}_Folds'] == fold, :
     ].index
     # update the parameters for the learning rate estimation with the split data
     lr_scan_params['train_data'] = (
-        predictors.loc[train_names,:].to_numpy()[...,np.newaxis],
-        targets.loc[train_names,compound],
+        predictors.loc[train_names, :].to_numpy()[..., np.newaxis],
+        targets.loc[train_names, compound],
     )
     # estimate the initial learning rate
     learning_rate_estimator.estimate_learnig_rate(
@@ -60,9 +77,7 @@ def cv_run(
             'Invalid model name'
         )(
             model_id='optimizer_cv',
-            **config.MODEL_PARAMS.get(cmd_args.model),
-            **config.SHARED_MODEL_PARAMS,
-            input_shape=(predictors.shape[1],1),
+            **model_params,
         ).build(),
         tried_optimizers=cv_utils.generate_optimizers(),
         **lr_scan_params
@@ -70,84 +85,86 @@ def cv_run(
     # loop over each explored optimizer
     for file_path in Path(config_cv_optimizers.RESULTS_PATH)\
         .joinpath('lr_estimates')\
-        .glob(f'{base_model_name}*.txt'):
-        with open(file_path,'r') as file:
+            .glob(f'{base_model_name}*.txt'):
+        with open(file_path, 'r') as file:
             lr_estimate = float(file.read())
         optimizer_name = '_'.join(
             file_path.name.split('_')[-2:]
-        ).replace('.txt','')
+        ).replace('.txt', '')
         print(f'{optimizer_name}:: {lr_estimate:.4f}')
         # take the current optimizer
         optimizer = explored_optimizers.get(optimizer_name)
-        if not optimizer: continue
+        if not optimizer:
+            continue
         optimizer.learning_rate = Variable(lr_estimate)
         # initialize w&b logging
-        wandb_run = wandb.init(
-            project=config.PROJECT_NAME,
-            name=f'{base_model.name}{optimizer_name}',
-            notes=f'fold_{fold}; {lr_estimate:.2e}, with weight reinit.'
-        )
-        wandb_run.define_metric(
-            name='val_root_mean_squared_error',
-            summary='min',
-            goal='minimize'
-        )
-        wandb_run.define_metric(
-            name='val_root_mean_squared_error',
-            summary='last',
-            goal='minimize'
-        )
+        if 'wandb' in selected_callbacks:
+            wandb_run = wandb.init(
+                project=config.PROJECT_NAME,
+                name=f'{base_model.name}{optimizer_name}',
+                notes=f'fold_{fold}; {lr_estimate:.2e}, with weight reinit.'
+            )
+            wandb_run.define_metric(
+                name='val_root_mean_squared_error',
+                summary='min',
+                goal='minimize'
+            )
+            wandb_run.define_metric(
+                name='val_root_mean_squared_error',
+                summary='last',
+                goal='minimize'
+            )
+
+            callbacks.append(
+                WandbCallback(
+                    log_weights=False,
+                    save_model=False
+                )
+            )
         # clone architecture to reset weights
         model = clone_model(base_model)
         model.compile(
             optimizer=optimizer,
-            loss='mse',
+            loss=model_params.get('loss_func'),
             metrics=[
                 RootMeanSquaredError(),
                 MeanAbsoluteError()
             ]
         )
         # create checkpoint directory
-        # checkpoint_path = CHECKPOINT_DIR.joinpath(
-        #     f'{model.name}{optimizer_name}_fold_{fold}/cp_lowest_validation_rmse.ckpt'
-        # )
+        if 'checkpointing' in selected_callbacks:
+            checkpoint_path = checkpoint_dir.joinpath(
+                f'{model.name}{optimizer_name}_fold_{fold}/cp_lowest_validation_rmse.ckpt'
+            )
+            if not checkpoint_path.parent.is_dir():
+                print('creating direction')
+                checkpoint_path.parent.mkdir()
+            else:
+                print('creating direction')
+            callback_checkpointing = ModelCheckpoint(
+                filepath=checkpoint_path,
+                save_weights_only=True,
+                verbose=1,
+                monitor='val_root_mean_squared_error',
+                mode='min',
+                save_best_only=True,
+                initial_value_threshold=start_checkpoint_at
+            )
 
-        # if not checkpoint_path.parent.is_dir():
-        #     print('creating direction')
-        #     checkpoint_path.parent.mkdir()
-        # else:
-        #     print('creating direction')
-
-        # initialize checkpoint callback
-        # callback_checkpointing = tf.keras.callbacks.ModelCheckpoint(
-        #     filepath=checkpoint_path,
-        #     save_weights_only=True,
-        #     verbose=1,
-        #     monitor='val_root_mean_squared_error',
-        #     mode='min',
-        #     save_best_only=True,
-        #     initial_value_threshold=10
-        # )
+            callbacks.append(callback_checkpointing)
         # fit the model
         model.fit(
-            x=predictors.loc[train_names,:]\
-              .to_numpy()[...,np.newaxis],
-            y=targets.loc[train_names,compound],
+            x=predictors.loc[train_names, :]
+            .to_numpy()[..., np.newaxis],
+            y=targets.loc[train_names, compound],
             batch_size=config.BATCH_SIZE,
             epochs=config_cv_optimizers.CV_EPOCHS,
             validation_data=(
-                predictors.loc[val_names,:]\
-                  .to_numpy()[...,np.newaxis],
-                targets.loc[val_names,compound]
+                predictors.loc[val_names, :]
+                .to_numpy()[..., np.newaxis],
+                targets.loc[val_names, compound]
             ),
-            callbacks=[
-                # callback_checkpointing,
-                WandbCallback(
-                    log_weights=False,
-                    save_model=False
-                ),
-                cv_utils.ReinitializeWeights()
-            ]
+            callbacks=callbacks
         )
 
 
@@ -173,6 +190,14 @@ if __name__ == '__main__':
         '--predictors',
         type=str,
     )
+    argument_parser.add_argument(
+        '--callbacks',
+        type=str,
+    )
+    argument_parser.add_argument(
+        '--kwargs',
+        type=str,
+    )
     cmd_args = argument_parser.parse_args()
 
     cv_run(
@@ -182,13 +207,17 @@ if __name__ == '__main__':
         predictors_path=cmd_args.predictors,
         targets_path=cmd_args.targets,
         lr_scan_params={
-            'results_path':Path(config_cv_optimizers.RESULTS_PATH),
-            'end_lr':config_cv_optimizers.LR_SCAN_END,
-            'step_size':config_cv_optimizers.LR_SCAN_STEP_SIZE,
-            'warmup_count':config_cv_optimizers.LR_SCAN_WARMUP,
-            'batch_size':config.BATCH_SIZE,
-            'overwrite_existing':True,
-            'return_data':False,
-            'save_fig':False
-        }
+            'results_path': Path(config_cv_optimizers.RESULTS_PATH),
+            'end_lr': config_cv_optimizers.LR_SCAN_END,
+            'step_size': config_cv_optimizers.LR_SCAN_STEP_SIZE,
+            'warmup_count': config_cv_optimizers.LR_SCAN_WARMUP,
+            'batch_size': config.BATCH_SIZE,
+            'overwrite_existing': True,
+            'return_data': False,
+            'save_fig': False
+        },
+        start_checkpoint_at=5.,
+        checkpoint_dir=Path(config.CHECKPOINT_DIR),
+        callbacks=cmd_args.callbacks,
+        **ast.literal_eval(cmd_args.kwargs),
     )
