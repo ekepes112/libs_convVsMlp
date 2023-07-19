47cc56641d8913086b2a39aa456418ebceb64cb6
ekepes112 <61498073+ekepes112@users.noreply.github.com>
2023-07-19 13:08:45 +0200

    functionality: added function to define pyplot defaults, added constants

--- src/graph_utils.py
+++ src/graph_utils.py
@@ -1,4 +1,10 @@
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
+CM = 1/2.54
+AXIS_LABEL_TEXT_SIZE = 8
+GRID_ALPHA = .1
+MARKER_ALPHA = .5
 
 
 def _update_layout(
@@ -41,3 +47,26 @@ def _update_layout(
             plot_bgcolor="#FFF"
         )
     )
+
+
+def set_pyplot_defaults(save_fig: bool = False) -> None:
+    plt.rcParams["figure.figsize"] = (
+        16 * CM, 12 * CM) if save_fig else (16, 12)
+    plt.rcParams['axes.titlesize'] = 8
+    plt.rcParams['axes.titleweight'] = 1
+    plt.rcParams['axes.titlecolor'] = 'black'
+    plt.rcParams['grid.alpha'] = .1
+    plt.rcParams['ytick.labelsize'] = 7
+    plt.rcParams['xtick.labelsize'] = 7
+    plt.rcParams['axes.grid'] = True
+    plt.rcParams['axes.titlelocation'] = 'left'
+    plt.rcParams['font.size'] = 8
+    plt.rcParams['legend.fontsize'] = 7
+    plt.rcParams['lines.linewidth'] = .35
+    plt.rcParams['savefig.transparent'] = True
+    plt.rcParams['savefig.pad_inches'] = 0
+    plt.rcParams['savefig.bbox'] = 'tight'
+    plt.rcParams['savefig.format'] = 'tiff'
+    plt.rcParams['savefig.dpi'] = 600
+    plt.rcParams['legend.markerscale'] = 3
+    plt.rcParams['lines.markersize'] = .4
