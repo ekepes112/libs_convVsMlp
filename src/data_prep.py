96e4625846825a9c1ad92b517ce2d9cf8a634475
ekepes112 <kepes.erik@gmail.com>
2023-08-23 13:29:47 +0200

    functionality: if folds are not in metadata, perform stratified k-fold split

--- src/data_prep.py
+++ src/data_prep.py
@@ -3,12 +3,65 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 from sklearn import preprocessing
+from sklearn.model_selection import StratifiedKFold
 import plotly.graph_objs as go
+import re
 
 import data_loaders
 import data_preprocessors
 import config
 import graph_utils
+
+
+def get_target_from_spectrum_id(spectrum_id: str):
+    return re.sub('^[0-9]{1,}_','',spectrum_id)
+
+def generate_folds(
+    concentrations: pd.DataFrame,
+    fold_count: int
+):
+    concentration_bins = pd.cut(
+        concentrations,
+        bins=100//fold_count,
+        right=True,
+        labels=None,
+        retbins=False,
+        precision=3,
+        include_lowest=False,
+        duplicates='raise',
+        ordered=True
+    ).astype(str)
+    concentration_bins = pd.DataFrame({
+        'bin':concentration_bins,
+        'target': [
+            get_target_from_spectrum_id(x)
+            for x
+            in concentration_bins.index
+        ]
+    })
+    fold_splitter = StratifiedKFold(
+        n_splits=fold_count,
+        random_state=97481,
+        shuffle=True
+    )
+    fold_splitter.get_n_splits(
+        concentration_bins.loc[~concentration_bins.duplicated(),'target'],
+        concentration_bins.loc[~concentration_bins.duplicated(),'bin']
+    )
+
+    fold_concentrations = []
+    for _, test_index in fold_splitter.split(
+        concentration_bins.loc[~concentration_bins.duplicated(),'target'],
+        concentration_bins.loc[~concentration_bins.duplicated(),'bin']
+    ):
+        fold_concentrations.append(
+            concentration_bins.loc[
+                ~concentration_bins.duplicated(),
+                'target'
+            ].iloc[test_index]
+        )
+
+    return fold_concentrations,concentration_bins
 
 
 if __name__ == '__main__':
@@ -53,7 +106,6 @@ if __name__ == '__main__':
     wvl = wvl[mask_cond]
     data = data.loc[:,mask_cond]
 
-
     data = data_preprocessors.normalize_area(
         data,
         wvl,
@@ -81,6 +133,24 @@ if __name__ == '__main__':
     fig = graph_utils._update_layout(fig)
     fig.show()
 
+    if not f"{cmd_args.compound}_Folds" in list(targets.columns):
+        folds, bins = generate_folds(
+            concentrations=targets[cmd_args.compound],
+            fold_count=len(config.TRAIN_FOLDS) + 1
+        )
+
+        for ndx,fold in enumerate(folds):
+            targets.loc[
+                bins['target'].isin(fold),
+                f"{cmd_args.compound}_Folds"
+            ] = int(ndx)
+
+            targets[
+                f"{cmd_args.compound}_Folds"
+            ] = targets[
+                f"{cmd_args.compound}_Folds"
+            ].astype(np.int8)
+
     targets = targets.loc[data.index]
     targets = targets.loc[
         (targets.loc[:,f'{cmd_args.compound}_Folds'] > 0) & \
@@ -88,7 +158,6 @@ if __name__ == '__main__':
           np.invert(np.isnan(targets.loc[:,cmd_args.compound])),
         :
     ]
-
     test_names = targets.loc[
         targets.loc[:,f'{cmd_args.compound}_Folds'] == config.TEST_FOLD,:
     ].index
